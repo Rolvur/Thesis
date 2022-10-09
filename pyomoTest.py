98a7970b704c06d9169335803d223b1c9088defe
Rolvur Reinert <s162601@student.dtu.dk>
2022-10-09 16:43:04 +0200

    pyomo-test

---
+++ pyomoTest.py
@@ -0,0 +1,34 @@
+from pyomo.environ import *
+#https://jckantor.github.io/CBE30338/06.04-Linear-Production-Model-in-Pyomo.html
+# create a model
+model = ConcreteModel()
+
+# declare decision variables
+model.x = Var(domain=NonNegativeReals)
+model.y = Var(domain=NonNegativeReals)
+
+# declare objective
+model.profit = Objective(expr = 40*model.x + 30*model.y, sense=maximize)
+
+# declare constraints
+model.demand = Constraint(expr = model.x <= 40)
+model.laborA = Constraint(expr = model.x + model.y <= 80)
+model.laborB = Constraint(expr = 2*model.x + model.y <= 100)
+
+# solve
+results = SolverFactory('glpk').solve(model)
+results.write()
+if results.solver.status:
+    model.pprint()
+
+# display solution
+print('\nProfit = ', model.profit())
+
+print('\nDecision Variables')
+print('x = ', model.x())
+print('y = ', model.y())
+
+print('\nConstraints')
+print('Demand  = ', model.demand())
+print('Labor A = ', model.laborA())
+print('Labor B = ', model.laborB())
