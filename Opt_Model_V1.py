fd3585997863d60336efff440a2b9ca0b725fb26
Mathias Mortensen <holbaek.mathias@outlook.dk>
2022-10-19 12:13:20 +0200

    edit model constants file

--- Opt_Model_V1.py
+++ Opt_Model_V1.py
@@ -4,8 +4,6 @@ import pyomo.opt as po
 from Opt_Constants import *
 #____________________________________________
 
-
-
 P_PV_max = {1:      0, 
             2:      0, 
             3:      0, 
@@ -108,8 +106,9 @@ model.k_CR = k_CR
 model.r_in = r_in
 model.r_out = r_out
 model.k_d = k_d
-model.S_pure_max = S_pure_max
+model.S_Pu_max = S_Pu_max
 model.S_raw_max = S_raw_max
+model.m_H2_max = m_H2_max
 model.ramp_pem = ramp_pem
 model.ramp_com = ramp_com
 model.P_PV_cap = P_PV_cap
@@ -120,9 +119,12 @@ model.p_PV = pe.Var(model.T, domain=pe.Reals)
 model.p_pem = pe.Var(model.T, domain=pe.Reals)
 model.m_H2 = pe.Var(model.T, domain=pe.Reals)
 model.m_CO2 = pe.Var(model.T, domain=pe.Reals)
+model.m_H2O = pe.Var(model.T, domain=pe.Reals)
 model.m_Ri = pe.Var(model.T, domain=pe.Reals)
-model.S_raw = pe.Var(model.T, domain=pe.Reals)
-model.S_pure = pe.Var(model.T, domain=pe.Reals)
+model.m_Ro = pe.Var(model.T, domain=pe.Reals)
+model.m_Pu = pe.Var(model.T, domain=pe.Reals)
+model.s_raw = pe.Var(model.T, domain=pe.Reals)
+model.s_Pu = pe.Var(model.T, domain=pe.Reals)
 
 expr = sum(model.DA[t]*model.p_grid[t] for t in model.T)
 model.objective = pe.Objective(sense = pe.minimize, expr=expr)
@@ -160,6 +162,93 @@ for t in model.T:
     lhs = model.p_PV[t]
     model.c3_2.add(lhs <= rhs)
 
+model.c4_1 = pe.ConstraintList()
+for t in model.T:
+    rhs = model.p_pem[t]
+    lhs = model.P_pem_min
+    model.c4_1.add(lhs <= rhs)
+
+model.c4_2 = pe.ConstraintList()
+for t in model.T:
+    lhs = model.p_pem[t]
+    rhs = model.P_pem_cap
+    model.c4_2.add(lhs <= rhs)
+
+model.c5 = pe.ConstraintList()
+for t in model.T:
+    model.c5.add(model.m_H2[t] == model.k_CR*model.p_pem[t])
+
+#model.c6 = pe.ConstraintList()
+#for t in model.T:
+#    model.c6.add(model.m_CO2[t] == model.r_in*model.m_H2[t])
+
+#model.c7 = pe.ConstraintList()
+#for t in model.T:
+#    model.c7.add(model.m_Ri[t] == model.m_H2[t] + model.m_CO2[t])
+
+#model.c8 = pe.ConstraintList()
+#for t in model.T:
+#    model.c8.add(model.m_Ro[t] == model.m_Pu[t] + model.m_H2O[t])
+
+#model.c9 = pe.ConstraintList()
+#for t in model.T:
+#    model.c9.add(model.m_Pu[t] == model.r_out * model.m_H2O[t])
+
+#model.c10 = pe.ConstraintList()
+#for t in model.T:
+#    model.c10.add(model.m_Pu[t] == model.k_d)
+
+#model.c11_1 = pe.ConstraintList()
+#for t in model.T:
+#    model.c11_1.add(0 <= model.s_raw[t])
+
+model.c11_2 = pe.ConstraintList()
+for t in model.T:
+    model.c11_2.add(model.s_raw[t] <= model.S_raw_max)
+
+model.c12 = pe.ConstraintList()
+for t in model.T:
+    if t >= 2:
+        model.c12.add(model.s_raw[t] == model.s_raw[t-1] + model.m_Ri[t] - model.m_Ro[t])
+
+model.c13 = pe.Constraint(expr=0.5*model.S_raw_max == model.s_raw[24])
+
+model.c14_1 = pe.ConstraintList()
+for t in model.T:
+    model.c14_1.add(0 <= model.s_Pu[t])
+
+model.c14_2 = pe.ConstraintList()
+for t in model.T:
+    model.c14_2.add(model.s_Pu[t] <= model.S_Pu_max)
+
+model.c15 = pe.ConstraintList()
+for t in model.T:
+    model.c15.add(model.s_Pu[1] == 0)
+
+model.c16 = pe.ConstraintList()
+for t in model.T:
+    if t >= 2:
+        model.c16.add(model.s_Pu[t] == model.s_Pu[t-1] + model.m_Pu[t] - model.m_demand[t])
+
+model.c17_1 = pe.ConstraintList()
+for t in model.T:
+    if t >= 2:
+        model.c17_1.add(-model.ramp_pem * model.P_pem_cap <= model.p_pem[t] - model.p_pem[t-1])
+
+model.c17_2 = pe.ConstraintList()
+for t in model.T:
+    if t >= 2:
+        model.c17_2.add(model.p_pem[t] - model.p_pem[t-1] <= model.ramp_pem * model.P_pem_cap)
+
+model.c18_1 = pe.ConstraintList()
+for t in model.T:
+    if t >= 2:
+        model.c18_1.add(-model.ramp_com * model.m_H2_max <= model.m_H2[t] - model.m_H2[t-1])
+
+model.c18_2 = pe.ConstraintList()
+for t in model.T:
+    if t >= 2:
+        model.c18_2.add(model.m_H2[t] - model.m_H2[t-1] <= model.ramp_com * model.m_H2_max)
 
 solver = po.SolverFactory('gurobi')
 results = solver.solve(model)
@@ -175,8 +264,24 @@ for i in model.p_PV:
   print(str(model.p_PV[i]), model.p_PV[i].value)
 for i in model.p_pem:
   print(str(model.p_pem[i]), model.p_pem[i].value)
-for i in model.p_pem:
-  print(str('P_com'), model.P_com)
-for i in model.p_pem:
-  print(str('P_H2O'), model.P_H2O)
+for i in model.m_H2:
+  print(str(model.m_H2[i]), model.m_H2[i].value)
 
+
+#for i in model.p_pem:
+#  print(str('P_com'), model.P_com)
+#for i in model.p_pem:
+#  print(str('P_H2O'), model.P_H2O)
+
+
+
+
+test=iter(model.T)
+next(test)
+for t in test:
+    print(t)
+
+
+    type(model.s_raw)
+    type(model.T)
+    S_Pu_max
