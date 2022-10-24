c1c48693ce053948387d848336da6eaf83094ed0
Rolvur Reinert <s162601@student.dtu.dk>
2022-10-24 09:17:15 +0200

    home work

--- Settings.py
+++ Settings.py
@@ -2,9 +2,9 @@
 
 
 Start_date = '2020-08-04 00:00'
-End_date = '2020-09-04 23:59 '
+End_date = '2020-08-07 23:59 '
 
-Demand_pattern = 'Weekly' # 'Hourly' , 'Daily' , 'Weekly'
+Demand_pattern = 'Daily' # 'Hourly' , 'Daily' , 'Weekly'
 
 
 
