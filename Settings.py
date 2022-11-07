ddce748e6e9224ae13deaf4f8506deedfa7d02bd
Rolvur Reinert <s162601@student.dtu.dk>
2022-11-07 15:24:37 +0100

    model update

--- Settings.py
+++ Settings.py
@@ -3,11 +3,11 @@
 
 
 
-Start_date = '2020-07-01 00:00'
+Start_date = '2020-08-01 00:00'
 End_date = '2020-08-31 23:59 '
 
 
-Demand_pattern = 'Daily' # 'Hourly' , 'Daily' , 'Weekly'
+Demand_pattern = 'Weekly' # 'Hourly' , 'Daily' , 'Weekly'
 
 
 
