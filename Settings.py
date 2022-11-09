d55fbbf75c5e7c512749a9ef52c7a282e1104864
Mathias Mortensen <holbaek.mathias@outlook.dk>
2022-11-09 11:16:22 +0100

    include variable efficiency

--- Settings.py
+++ Settings.py
@@ -3,8 +3,8 @@
 
 
 
-Start_date = '2020-01-01 00:00'
-End_date = '2021-12-31 23:59 '
+Start_date = '2020-08-01 00:00'
+End_date = '2020-08-14 23:59 '
 
 
 Demand_pattern = 'Weekly' # 'Hourly' , 'Daily' , 'Weekly'
