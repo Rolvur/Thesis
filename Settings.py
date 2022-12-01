15d7d429edef8cfaa9f1c8cc315e56745ab30c07
Rolvur Reinert <s162601@student.dtu.dk>
2022-12-01 14:55:41 +0100

    date split

--- Settings.py
+++ Settings.py
@@ -1,8 +1,24 @@
+
+### Model Simulation ### 
 Start_date = '2020-01-01 00:00'
 End_date = '2020-03-10 23:59'
 
 Demand_pattern = 'Weekly' # 'Hourly' , 'Daily' , 'Weekly'
 sEfficiency = 'k' # 'k': constant OR 'pw': piecewise 
+
+
+### Scenarios ### 
+Start_date_scen = '2020-01-01 00:00'
+End_date_scen = '2020-02-10 23:59'
+
+## Scenario Generation ## 
+Type = 'single' # single or combined # 
+n_samples = 10000 #Number of samples to be made  
+blocksize = 24 # 7days = 168 hours
+sample_length = blocksize*7 # sampling 52 weeks blocks 
+
+## Scenario Reduction ## 
+n_clusters = 5
 
 
 
