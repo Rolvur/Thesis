8e6c070c6b580889210e678c0590033f95a4c8fb
Mathias Mortensen <holbaek.mathias@outlook.dk>
2022-12-13 16:01:16 +0100

    Complete 'Economics'

--- Economics.py
+++ Economics.py
@@ -13,40 +13,30 @@ import os
 
 #---FUNCTION DEFINITIONS--------------------------------------------------------
 
-#Import and combine SolX files
-## Reading Rep Weeks 
-file_to_open = Path("Result_files/") / 'RepWeeks.xlsx'
-df_RepWeeks = pd.read_excel(file_to_open)
-#df_RepWeeks.columns[2]
-#df_RepWeeks.columns[3]
-#df_RepWeeks.columns[4]
-
-
-find_model='V3_SolX'
-find_year = '2021'
-avoid = 'year'
-find_unique = 'V'
-#Import and merge all files in "path" containing "find_year, "find_model" and "find_unique" in file name
-
 def findRepWeek(f,year):
     print('f: ')
     print(f)
-    file_to_open = Path("Result_files/") / 'RepWeeks.xlsx'
-    df_RepWeeks = pd.read_excel(file_to_open)
-    if year == '2020':
-        for i in range(0,len(df_RepWeeks)):
-            if str(df_RepWeeks[df_RepWeeks.columns[1]][i])[:10] in f:
-                π = df_RepWeeks[df_RepWeeks.columns[2]][i]
-                return i,π
-    elif year == '2021':
-        for i in range(0,len(df_RepWeeks)):
-            if str(df_RepWeeks[df_RepWeeks.columns[3]][i])[:10] in f:
-                π = df_RepWeeks[df_RepWeeks.columns[4]][i]
-                print('f: ')
-                print(f)
-                print('pi: ')
-                print(π)
-                return i,π
+    if '12-31' in f and '01-01' in f: #OBS!
+        π = 1
+        i = 0
+        return π,i
+    else:
+        file_to_open = Path("Result_files/") / 'RepWeeks.xlsx'
+        df_RepWeeks = pd.read_excel(file_to_open)
+        if year == '2020':
+            for i in range(0,len(df_RepWeeks)):
+                if str(df_RepWeeks[df_RepWeeks.columns[1]][i])[:10] in f:
+                    π = df_RepWeeks[df_RepWeeks.columns[2]][i]
+                    return i,π
+        elif year == '2021':
+            for i in range(0,len(df_RepWeeks)):
+                if str(df_RepWeeks[df_RepWeeks.columns[3]][i])[:10] in f:
+                    π = df_RepWeeks[df_RepWeeks.columns[4]][i]
+                    print('f: ')
+                    print(f)
+                    print('pi: ')
+                    print(π)
+                    return i,π
 
 def import_w_model_results(path,find_year,find_model, find_unique, avoid):
     files = os.listdir(path)
@@ -58,21 +48,23 @@ def import_w_model_results(path,find_year,find_model, find_unique, avoid):
     for f in files_xls:
         if (find_year in f) and (find_model in f) and (find_unique in f):
             if not f.startswith("~"):
-                if not f.endswith('12-31.xlsx'): #OBS!
-                    print(f)
-                    i,π = findRepWeek(f,find_year)
-                    list_f.append(f)
-                    list_pi.append(π)
-                    list_i.append(i)
-                    data = pd.read_excel(path+f)
-                    df_import = df_import.append(data)
-    df_import.index = (np.arange(0,1680,1))
+                # only necessary for weeks: if not f.endswith('12-31.xlsx'): #OBS!
+                print('file: '+f)
+                i,π = findRepWeek(f,find_year)
+                list_f.append(f)
+                list_pi.append(π)
+                list_i.append(i)
+                data = pd.read_excel(path+f)
+                df_import = df_import.append(data)
+    # only needed if weeks: df_import.index = (np.arange(0,1680,1))
     #print(list_pi)
     #print(list_i)
-    print(list_i)
-    print(list_f)
-    print(list_pi)
     #print(list_i)
+    #print(list_f)
+    #print(list_pi)
+    #print(df_import['c_DA'])
+    #print(list_i)
+    #df_import.to_excel("import_data"+find_year+".xlsx")
     return df_import, list_pi
 #Import LAST file in "path" containing "find_year, "find_model" and "find_unique" in file name
 def import_model_param(path,find_year,find_model, find_unique):
@@ -125,9 +117,15 @@ def generate_vOPEX(path,find_model,find_year,find_unique,avoid):
                 #All_Data[dict_key]['aFRRdown_revenue'] = All_Data[dict_key]['r_aFRR_down']*All_Data[dict_key]['c_aFRR_down']
                 #All_Data[dict_key]['mFRRdown_revenue'] = All_Data[dict_key]['r_mFRR_up']*All_Data[dict_key]['c_mFRR_up']
                 #type in DataFrame column naming in model V2 - all results could be rerun but not done at this point
-                if find_model[m] == 'V2':            
+                if find_model[m] == 'V2':
+                    print('V2 here: ')
+                    print(All_Data[dict_key]['c_DA'])
+#                    print('V2 here: ')
+                                
                     All_Data[dict_key]['DA_revenue'] = All_Data[dict_key]['P_export']*All_Data[dict_key]['c_DA']
+                    print(sum(All_Data[dict_key]['DA_revenue']))
                     All_Data[dict_key]['DA_expenses'] = All_Data[dict_key]['P_import']*All_Data[dict_key]['c_DA']
+                    print(sum(All_Data[dict_key]['DA_expenses']))
                     All_Data[dict_key]['aFRRdown_revenue'] = All_Data[dict_key]['r_aFRR_down']*All_Data[dict_key]['c_aFRR_down']
                     All_Data[dict_key]['mFRRup_revenue'] = All_Data[dict_key]['r_mFRR_up']*All_Data[dict_key]['c_mFRRup']
 
@@ -139,29 +137,41 @@ def generate_vOPEX(path,find_model,find_year,find_unique,avoid):
     return All_Data, All_Pi
 
 # Calculate yearly values based on the dictionary 
-def calc_vOPEX_year(find_model, find_year,All_Data, All_Pi):
+def calc_vOPEX_year(find_model, find_year,All_Data, All_Pi,data_length):
     vOPEX_year = {}
     for m in range(0,len(find_model)):
         for y in range(0,len(find_year)):
             dict_key = 'df_'+find_model[m]+'_'+find_year[y]
             print(dict_key)
             vOPEX_year[dict_key] = {}
-            for π in range(0,len(All_Pi[dict_key])):
-                print(sum(All_Data[dict_key]['DA_revenue'][π*168:(π+1)*168])*All_Pi[dict_key][π]*(365/7))
-            vOPEX_year[dict_key]['DA_revenue'] = sum( sum(All_Data[dict_key]['DA_revenue'][π*168:(π+1)*168])*All_Pi[dict_key][π] for π in range(0,len(All_Pi[dict_key]))) * (365/7)
-            vOPEX_year[dict_key]['DA_revenue_old'] = sum(All_Data[dict_key]['DA_revenue'])*((365*24)/len(All_Data[dict_key]))
-            vOPEX_year[dict_key]['DA_expenses'] = sum(All_Data[dict_key]['DA_expenses'])*((365*24)/len(All_Data[dict_key]))
-            vOPEX_year[dict_key]['CT_expenses'] = sum(All_Data[dict_key]['CT_expenses'])*((365*24)/len(All_Data[dict_key]))
-            vOPEX_year[dict_key]['PT_expenses'] = sum(All_Data[dict_key]['PT_expenses'])*((365*24)/len(All_Data[dict_key]))
-            if find_model[m] != 'V1':
-                vOPEX_year[dict_key]['aFRRup_revenue'] = sum(All_Data[dict_key]['aFRRup_revenue'])*((365*24)/len(All_Data[dict_key]))
-                vOPEX_year[dict_key]['aFRRdown_revenue'] = sum(All_Data[dict_key]['aFRRdown_revenue'])*((365*24)/len(All_Data[dict_key]))
-                vOPEX_year[dict_key]['mFRRup_revenue'] = sum(All_Data[dict_key]['mFRRup_revenue'])*((365*24)/len(All_Data[dict_key]))
-                vOPEX_year[dict_key]['FCR_revenue'] = sum(All_Data[dict_key]['FCR_revenue'])*((365*24)/len(All_Data[dict_key]))
+            if data_length == 'year':
+                print('data_length = year')
+                print('DA_revenue: ')
+                vOPEX_year[dict_key]['DA_revenue'] = sum(All_Data[dict_key]['DA_revenue'])*((365*24)/len(All_Data[dict_key]))
+                print(vOPEX_year[dict_key]['DA_revenue'])
+                vOPEX_year[dict_key]['DA_expenses'] = sum(All_Data[dict_key]['DA_expenses'])*((365*24)/len(All_Data[dict_key]))
+                vOPEX_year[dict_key]['CT_expenses'] = sum(All_Data[dict_key]['CT_expenses'])*((365*24)/len(All_Data[dict_key]))
+                vOPEX_year[dict_key]['PT_expenses'] = sum(All_Data[dict_key]['PT_expenses'])*((365*24)/len(All_Data[dict_key]))
+                if find_model[m] != 'V1':
+                    vOPEX_year[dict_key]['aFRRup_revenue'] = sum(All_Data[dict_key]['aFRRup_revenue'])*((365*24)/len(All_Data[dict_key]))
+                    vOPEX_year[dict_key]['aFRRdown_revenue'] = sum(All_Data[dict_key]['aFRRdown_revenue'])*((365*24)/len(All_Data[dict_key]))
+                    vOPEX_year[dict_key]['mFRRup_revenue'] = sum(All_Data[dict_key]['mFRRup_revenue'])*((365*24)/len(All_Data[dict_key]))
+                    vOPEX_year[dict_key]['FCR_revenue'] = sum(All_Data[dict_key]['FCR_revenue'])*((365*24)/len(All_Data[dict_key]))
+            if data_length == 'week':
+                vOPEX_year[dict_key]['DA_revenue'] = sum(sum(All_Data[dict_key]['DA_revenue'][π*168:(π+1)*168])*All_Pi[dict_key][π] for π in range(0,len(All_Pi[dict_key]))) * (365/7)
+                vOPEX_year[dict_key]['DA_expenses'] = sum(sum(All_Data[dict_key]['DA_expenses'][π*168:(π+1)*168])*All_Pi[dict_key][π] for π in range(0,len(All_Pi[dict_key]))) * (365/7)            
+                vOPEX_year[dict_key]['CT_expenses'] = sum(sum(All_Data[dict_key]['CT_expenses'][π*168:(π+1)*168])*All_Pi[dict_key][π] for π in range(0,len(All_Pi[dict_key]))) * (365/7)
+                vOPEX_year[dict_key]['PT_expenses'] = sum(sum(All_Data[dict_key]['PT_expenses'][π*168:(π+1)*168])*All_Pi[dict_key][π] for π in range(0,len(All_Pi[dict_key]))) * (365/7)
+                if find_model[m] != 'V1':
+                    vOPEX_year[dict_key]['aFRRup_revenue'] = sum(sum(All_Data[dict_key]['aFRRup_revenue'][π*168:(π+1)*168])*All_Pi[dict_key][π] for π in range(0,len(All_Pi[dict_key]))) * (365/7)
+                    vOPEX_year[dict_key]['aFRRdown_revenue'] = sum(sum(All_Data[dict_key]['aFRRdown_revenue'][π*168:(π+1)*168])*All_Pi[dict_key][π] for π in range(0,len(All_Pi[dict_key]))) * (365/7)
+                    vOPEX_year[dict_key]['mFRRup_revenue'] = sum(sum(All_Data[dict_key]['mFRRup_revenue'][π*168:(π+1)*168])*All_Pi[dict_key][π] for π in range(0,len(All_Pi[dict_key]))) * (365/7)
+                    vOPEX_year[dict_key]['FCR_revenue'] = sum(sum(All_Data[dict_key]['FCR_revenue'][π*168:(π+1)*168])*All_Pi[dict_key][π] for π in range(0,len(All_Pi[dict_key]))) * (365/7)    
     return vOPEX_year
 
 # Combine vOPEX with fOPEX and CAPEX - AND - discount/summations etc.
 def Econ_Data_Constructor(dfEconParam,dict_vOPEX):
+    print(dict_vOPEX)
     N = dfEconParam['lifetime'].iloc[0]
     n = [2023+i for i in range(0,N+1)]
     #CAPEX
@@ -189,32 +199,32 @@ def Econ_Data_Constructor(dfEconParam,dict_vOPEX):
     fOPEX_CO2[0] = 0
     fOPEX_CO2_disc = [fOPEX_CO2[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)]
     #vOPEX (from model results)
-    vOPEX_DA_revenue = [-dict_vOPEX['DA_revenue'] for i in range(0,N+1)]
+    vOPEX_DA_revenue = [-dict_vOPEX['DA_revenue']/10**6 for i in range(0,N+1)]
     vOPEX_DA_revenue[0] = 0
     vOPEX_DA_revenue_disc = [vOPEX_DA_revenue[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)] 
-    vOPEX_DA_revenue_old = [-dict_vOPEX['DA_revenue_old'] for i in range(0,N+1)]
-    vOPEX_DA_revenue_old[0] = 0
-    vOPEX_DA_revenue_old_disc = [vOPEX_DA_revenue_old[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)] 
-    vOPEX_DA_expenses = [dict_vOPEX['DA_expenses'] for i in range(0,N+1)]
+    #vOPEX_DA_revenue_old = [-dict_vOPEX['DA_revenue_old'] for i in range(0,N+1)]
+    #vOPEX_DA_revenue_old[0] = 0
+    #vOPEX_DA_revenue_old_disc = [vOPEX_DA_revenue_old[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)] 
+    vOPEX_DA_expenses = [dict_vOPEX['DA_expenses']/10**6 for i in range(0,N+1)]
     vOPEX_DA_expenses[0] = 0
     vOPEX_DA_expenses_disc = [vOPEX_DA_expenses[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)]
-    vOPEX_CT = [dict_vOPEX['CT_expenses'] for i in range(0,N+1)]
+    vOPEX_CT = [dict_vOPEX['CT_expenses']/10**6 for i in range(0,N+1)]
     vOPEX_CT[0] = 0
     vOPEX_CT_disc = [vOPEX_CT[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)]
-    vOPEX_PT = [dict_vOPEX['PT_expenses'] for i in range(0,N+1)]
+    vOPEX_PT = [dict_vOPEX['PT_expenses']/10**6 for i in range(0,N+1)]
     vOPEX_PT[0] = 0
     vOPEX_PT_disc = [vOPEX_PT[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)]
     if 'FCR_revenue' in dict_vOPEX:
-        vOPEX_FCR = [-dict_vOPEX['FCR_revenue'] for i in range(0,N+1)]
+        vOPEX_FCR = [-dict_vOPEX['FCR_revenue']/10**6 for i in range(0,N+1)]
         vOPEX_FCR[0] = 0
         vOPEX_FCR_disc = [vOPEX_FCR[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)]
-        vOPEX_aFRRup = [-dict_vOPEX['aFRRup_revenue'] for i in range(0,N+1)]
+        vOPEX_aFRRup = [-dict_vOPEX['aFRRup_revenue']/10**6 for i in range(0,N+1)]
         vOPEX_aFRRup[0] = 0
         vOPEX_aFRRup_disc = [vOPEX_aFRRup[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)]
-        vOPEX_aFRRdown = [-dict_vOPEX['aFRRdown_revenue'] for i in range(0,N+1)]
+        vOPEX_aFRRdown = [-dict_vOPEX['aFRRdown_revenue']/10**6 for i in range(0,N+1)]
         vOPEX_aFRRdown[0] = 0
         vOPEX_aFRRdown_disc = [vOPEX_aFRRdown[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)]
-        vOPEX_mFRRup = [-dict_vOPEX['mFRRup_revenue'] for i in range(0,N+1)]
+        vOPEX_mFRRup = [-dict_vOPEX['mFRRup_revenue']/10**6 for i in range(0,N+1)]
         vOPEX_mFRRup[0] = 0
         vOPEX_mFRRup_disc = [vOPEX_mFRRup[t]/(1+dfEconParam['discount rate'].iloc[0])**t for t in range(0,N+1)]
 
@@ -241,7 +251,7 @@ def Econ_Data_Constructor(dfEconParam,dict_vOPEX):
                                 'METH_fOPEX' : fOPEX_METHANOL,
                                 'CO2_fOPEX' : fOPEX_CO2,
                                 'vOPEX_DA_revenue' : vOPEX_DA_revenue,
-                                'vOPEX_DA_revenue_old' : vOPEX_DA_revenue_old,
+                                #'vOPEX_DA_revenue_old' : vOPEX_DA_revenue_old,
                                 'vOPEX_DA_expenses' : vOPEX_DA_expenses, 
                                 'vOPEX_CT' : vOPEX_CT,
                                 'vOPEX_PT' : vOPEX_PT,
@@ -289,12 +299,18 @@ path = "Result_files/"  # the "/" is important!
 #   define to different types of files to be analyzed
 #find_year = ['2020','2021']
 find_year = ['2020','2021']
-find_model = ['V3_SolX']
-find_unique = 'V' 
+find_model = ['V2']
+find_unique = 'year' 
+data_length = 'year' #'week' or 'year'
 avoid = 'pw'
 
+# Reading Rep Weeks
+if data_length == 'week': 
+    file_to_open = Path("Result_files/") / 'RepWeeks.xlsx'
+    df_RepWeeks = pd.read_excel(file_to_open)
+
 All_Data, All_Pi = generate_vOPEX(path,find_model,find_year,find_unique,avoid)
-vOPEX_year = calc_vOPEX_year(find_model, find_year,All_Data, All_Pi)
+vOPEX_year = calc_vOPEX_year(find_model, find_year,All_Data, All_Pi, data_length)
 dfEconParam = pd.read_excel("Data/Economics_Data.xlsx")
 
 #-------------WRITE CFA YEARLY RESULTS TO EXCEL SHEETS ----------------
@@ -304,9 +320,8 @@ for m in range(0,len(find_model)):
         dict_key = 'df_'+find_model[m]+'_'+find_year[y]
         # Write DataFrame to Excel file with sheet name
         df_Econ = Econ_Data_Constructor(dfEconParam,vOPEX_year[dict_key])
-        df_Econ.to_excel(writer, sheet_name=dict_key)
+        df_Econ.to_excel(writer, sheet_name=dict_key+'_'+data_length)
 writer.save()
-
 
 
 
