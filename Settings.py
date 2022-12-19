e8c1eb269cf4e5a247dd0757d10d6933070c73db
Rolvur Reinert <s162601@student.dtu.dk>
2022-12-19 13:02:41 +0100

    sim done

--- Settings.py
+++ Settings.py
@@ -2,19 +2,19 @@
 
 
 ### Model Simulation & PV ### 
-Start_date = '2020-01-13 00:00'
-End_date = '2020-01-19 23:59'
+Start_date = '2020-01- 00:00'
+End_date = '2020-09-12 23:59'
 
 Demand_pattern = 'Weekly' # 'Hourly' , 'Daily' , 'Weekly'
 sEfficiency = 'k' # 'k': constant OR 'pw': piecewise 
 
 #--------------------------------------------------------------------------
 ### Scenarios ### 
-Start_date_scen = '2020-01-01 00:00'
-End_date_scen = '2020-01-12 23:59'
+Start_date_scen = '2021-08-09 00:00'
+End_date_scen = '2021-09-05 23:59'
 
 ## Scenario Generation ## 
-Type = 'single' # single or combined # 
+Type = 'combined' # single or combined # 
 n_samples = 10000 #Number of samples to be made  
 blocksize = 24 # 7days = 168 hours
 sample_length = blocksize*7 # sampling 52 weeks blocks 
